4bcde8121c22047d7c6639f6771f320acc62f54e
Tom Koptel <tom.koptel@gmail.com>
2021-06-02 19:32:25 +0200

    Revert changes in NetworkInterceptor.kt

--- app/src/main/java/com/olderwold/reddit/lib/okhttp/NetworkInterceptor.kt
+++ app/src/main/java/com/olderwold/reddit/lib/okhttp/NetworkInterceptor.kt
@@ -3,28 +3,24 @@ package com.olderwold.reddit.lib.okhttp
 import okhttp3.CacheControl
 import okhttp3.Interceptor
 import okhttp3.Response
-import retrofit2.Invocation
 import java.util.concurrent.TimeUnit
 
 class NetworkInterceptor : Interceptor {
+    private companion object {
+        const val MAX_AGE = 5
+    }
+
     override fun intercept(chain: Interceptor.Chain): Response {
-        val request = chain.request()
-        val invocation: Invocation? = request.tag(Invocation::class.java)
-        val annotation: Cacheable? = invocation?.method()?.getAnnotation(Cacheable::class.java)
-
         val response = chain.proceed(chain.request())
-        return if (annotation == null) {
-            response
-        } else {
-            val cacheControl = CacheControl.Builder()
-                .maxAge(annotation.until, annotation.unit)
-                .build()
 
-            return response.newBuilder()
-                .removeHeader(HEADER_PRAGMA)
-                .removeHeader(HEADER_CACHE_CONTROL)
-                .header(HEADER_CACHE_CONTROL, cacheControl.toString())
-                .build()
-        }
+        val cacheControl = CacheControl.Builder()
+            .maxAge(MAX_AGE, TimeUnit.SECONDS)
+            .build()
+
+        return response.newBuilder()
+            .removeHeader(HEADER_PRAGMA)
+            .removeHeader(HEADER_CACHE_CONTROL)
+            .header(HEADER_CACHE_CONTROL, cacheControl.toString())
+            .build()
     }
 }
