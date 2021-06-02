27c90cf4a248542d86f103cfe422454f14925150
Tom Koptel <tom.koptel@gmail.com>
2021-06-02 18:08:13 +0200

    Cover additional branch in WebViewPageTest

--- app/src/test/java/com/olderwold/reddit/feed/WebViewPageTest.kt
+++ app/src/test/java/com/olderwold/reddit/feed/WebViewPageTest.kt
@@ -1,10 +1,13 @@
 package com.olderwold.reddit.feed
 
 import android.app.Activity
+import android.content.ActivityNotFoundException
 import android.content.Intent
+import io.mockk.every
 import io.mockk.mockk
 import io.mockk.spyk
 import io.mockk.verify
+import org.junit.Assert.fail
 import org.junit.Test
 
 class WebViewPageTest {
@@ -16,10 +19,21 @@ class WebViewPageTest {
     }
 
     @Test
-    fun open() {
+    fun `there is an app that can handle intent`() {
         webViewPage.open("https://cutepuppies")
 
         verify { intentFactory.invoke("https://cutepuppies") }
         verify { activity.startActivity(mockIntent) }
+    }
+
+    @Test
+    fun `there is no app that can handle intent`() {
+        every { activity.startActivity(any()) } throws ActivityNotFoundException()
+
+        try {
+            webViewPage.open("https://cutepuppies")
+        } catch (ex: Exception) {
+            fail("Should not throw $ex")
+        }
     }
 }
