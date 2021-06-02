dcb2f04a9473b0ca3e2badb82851f2cb3c820778
Tom Koptel <tom.koptel@gmail.com>
2021-06-02 17:24:22 +0200

    Connect ChromeTab

--- app/src/main/java/com/olderwold/reddit/MainActivity.kt
+++ app/src/main/java/com/olderwold/reddit/MainActivity.kt
@@ -6,20 +6,25 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import com.olderwold.reddit.feed.ChromeTab
 import com.olderwold.reddit.ui.element.RedditHotList
 import com.olderwold.reddit.ui.theme.YetanotherredditTheme
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     private val mainViewModel: MainViewModel by viewModels()
+
+    @Inject
+    internal lateinit var chromeTab: ChromeTab
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             YetanotherredditTheme {
                 Surface(color = MaterialTheme.colors.background) {
-                    RedditHotList(mainViewModel)
+                    RedditHotList(mainViewModel, chromeTab)
                 }
             }
         }
